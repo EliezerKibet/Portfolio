49dd456bca74d6e8b6323d227352f84749a93ce8
EliezerKibet <elieserkibet@gmail.com>
2025-12-19 11:03:42 +0100

    SItemap and Robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://eliezerkibet.vercel.app/"; // REPLACE with your actual Vercel URL
-
+  const baseUrl = "https://eliezerkibet.vercel.app/";
   return [
     {
       url: baseUrl,
